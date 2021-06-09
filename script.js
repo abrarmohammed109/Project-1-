9f2bacc389785ab7262c311a9f02fc3255647359
abrarmohammed109 <moasib.abrar@gmail.com>
2021-06-09 12:52:32 -0400

    Day 3 - switch to next question/answer choices upon answering previous question, stuck updating submit button

--- script.js
+++ script.js
@@ -103,16 +103,22 @@ const playerQuestionOne = (data) => {
     // }
 
     const gameHTML = `<h1 id='score'> ${me.name}'s turn. SCORE: ${me.score}</h1>
-    <h2>${me.Q_A.questions[0]}</h2>
+    <h2 id='question'>${me.Q_A.questions[0]}</h2>
     <form id='form'>
-    <input id='option1' type="radio" name="choice" value="">${me.Q_A.answers[0][0]}<br>
-    <input id='option2' type="radio" name="choice" value="">${me.Q_A.answers[0][1]}<br>
-    <input id='option3' type="radio" name="choice" value="choice3">${me.Q_A.answers[0][2]}<br>
-    <input id='option4' type="radio" name="choice" value="choice4">${me.Q_A.answers[0][3]}<br>
-    <button type='button' onclick="checkAnswer()"> LESGOOOO </button>
+    <input id='option1' type="radio" name="choice1" value="choice1">
+    <label id="choice1" for="choice1">${me.Q_A.answers[0][0]}</label><br>
+    <input id='option2' class ='next-question' type="radio" name="choice" value="">
+    <label id="choice2" for="choice1">${me.Q_A.answers[0][1]}</label><br>
+    <input id='option3' class ='next-question' type="radio" name="choice" value="choice3">
+    <label id="choice3" for="choice1">${me.Q_A.answers[0][2]}</label><br>
+    <input id='option4' class ='next-question' type="radio" name="choice" value="choice4">
+    <label id="choice4" for="choice1">${me.Q_A.answers[0][3]}</label><br>
+    <button id='submit' type='button' onclick="checkAnswer()"> LESGOOOO </button>
     </form>`
+
     mainGame.innerHTML = gameHTML
-   
+  
+    // <input id='option1' class ='next-question' type="radio" name="choice" value="">${me.Q_A.answers[0][0]}<br>
 
 
 }
@@ -127,7 +133,7 @@ const checkAnswer = () => {
         updateScore.innerHTML = `${me.name}'s turn. SCORE: ${me.score}`
         score.parentNode.replaceChild(updateScore,score)
 
-        //playerQuestionTwo()
+        playerQuestionTwo()
 
     } else if (document.getElementById('option4').checked){
         console.log('correct!')
@@ -139,17 +145,102 @@ const checkAnswer = () => {
         score.parentNode.replaceChild(updateScore,score)
         console.log(me.score)
 
-        //playerQuestionTwo()
+        playerQuestionTwo()
     }
 }
 
-/* ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆ END OF QUESTION ONE ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆/
+/* ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆ END OF QUESTION ONE FOR PLAYER 1⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆/
 
 
 
-/* ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇ QUESTION 2 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇*/
+/* ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇ QUESTION 2 FOR PLAYER 1 ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇*/
 
+const playerQuestionTwo = () => {
+    console.log(me.score)
+    
+    const questionreplace = document.getElementById('question') //replacing previous question with next question 
+    const updateQuestion = document.createElement('h2')
+    updateQuestion.innerHTML = me.Q_A.questions[1]   //replace the index here with the next number (max 9 or 19)
+    questionreplace.parentNode.replaceChild(updateQuestion,questionreplace)
 
+    const answerChoiceReplace1 = document.getElementById('choice1')  //replacing previous answers with next answers respective of the above replacing question
+    const updatedAnswerChoice1 = document.createElement('label')
+    updatedAnswerChoice1.innerHTML = me.Q_A.answers[2][0]  //replace second index with next number; on the 4th choice replace first index with next number and no second index
+    answerChoiceReplace1.parentNode.replaceChild(updatedAnswerChoice1,answerChoiceReplace1)
+    
+    const answerChoiceReplace2 = document.getElementById('choice2')  //replacing previous answers with next answers respective of the above replacing question
+    const updatedAnswerChoice2 = document.createElement('label')
+    updatedAnswerChoice2.innerHTML = me.Q_A.answers[2][1]  //replace second index with next number; on the 4th choice replace first index with next number and no second index
+    answerChoiceReplace2.parentNode.replaceChild(updatedAnswerChoice2,answerChoiceReplace2)
+
+    const answerChoiceReplace3 = document.getElementById('choice3')  //replacing previous answers with next answers respective of the above replacing question
+    const updatedAnswerChoice3 = document.createElement('label')
+    updatedAnswerChoice3.innerHTML = me.Q_A.answers[2][2]  //replace second index with next number; on the 4th choice replace first index with next number and no second index
+    answerChoiceReplace3.parentNode.replaceChild(updatedAnswerChoice3,answerChoiceReplace3)
+    
+    const answerChoiceReplace4 = document.getElementById('choice4')  //replacing previous answers with next answers respective of the above replacing question
+    const updatedAnswerChoice4 = document.createElement('label')
+    updatedAnswerChoice4.innerHTML = me.Q_A.answers[3]  //replace second index with next number; on the 4th choice replace first index with next number and no second index
+    answerChoiceReplace4.parentNode.replaceChild(updatedAnswerChoice4,answerChoiceReplace4)
+    
+    
+    const previousSubmit = document.getElementById('submit')
+    const form = document.getElementById('form')
+    previousSubmit.parentNode.removeChild(previousSubmit)
+    const newButton = document.createElement('button')
+    newButton.setAttribute('id','new-Submit')
+    newButton.setAttribute('onClick','playerQuestionThree()')
+    newButton.innerHTML = `SUBMIT`
+    form.appendChild(newButton)
+
+    // newButton.addEventListener('submit',playerQuestionThree)
+
+    // const form = document.getElementById('form')
+    // const newSubmitButton = document.getElementById('button')
+    // newSubmitButton.innerHTML = `<button id='submit' type='button' onclick="playerQuestionThree()"> SUBMIT </button>`
+    
+
+    
+    // const submitButton = document.getElementById('submit')
+    // const newSubmit = document.createElement('button')
+    // newSubmit.setAttribute('onclick',checkAnswer2())
+    // newSubmit.parentNode.replaceChild(newSubmit,submitButton)
+    
+    // if(document.getElementById('option1').checked || document.getElementById('option2').checked || document.getElementById('option3').checked){
+    //     alert('incorrect')
+    //     me.decreaseScore()
+
+    //     const score = document.getElementById('score')
+    //     const updateScore = document.createElement('h1')
+    //     updateScore.innerHTML = `${me.name}'s turn. SCORE: ${me.score}`
+    //     score.parentNode.replaceChild(updateScore,score)
+
+    //     playerQuestionThree()
+
+    // } else if (document.getElementById('option4').checked){
+    //     console.log('correct!')
+    //     me.increaseScore()
+
+    //     const score = document.getElementById('score')
+    //     const updateScore = document.createElement('h1')
+    //     updateScore.innerHTML = `${me.name}'s turn. SCORE: ${me.score}`
+    //     score.parentNode.replaceChild(updateScore,score)
+    //     console.log(me.score)
+
+    //     playerQuestionThree()
+    // }
+    
+    
+    
+    
+    console.log(me.Q_A.answers[2])
+   
+
+}
+
+const playerQuestionThree = () => {
+    console.log('NEXT ROUND')
+}
 
 
 
@@ -188,3 +279,5 @@ const checkAnswer = () => {
 
 
 button.addEventListener('click',beginGame)
+// const buttonSubmit = document.getElementById('submit')
+// buttonSubmit.addEventListener('click',checkAnswer)
